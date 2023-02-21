ca3e8604d423db21f6ab0d4a33abf1d467b92980
ColeRElfstrom <elfstromcole@gmail.com>
2023-02-21 17:52:41 +0000

    first push

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.write("""
+    # My First Streamlit Webpage
+
+    and some more text
+    """)
+
+df = pd.read_csv('StreamlitFun/data.csv')
+df
+
+options = st.selection()
+
+if(st.checkbox("Show Plotting Feature:")):
+    sample_df = pd.DataFrame(
+        np.random.randn(20, 3), columns=['a', 'b', 'c']
+    )
+    st.line_chart(sample_df)
+    
+
+myslider = st.slider("myslider")
+st.write(2 * myslider)
+
+
